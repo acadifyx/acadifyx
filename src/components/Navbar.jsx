8911c00169f5bbc140c33195d2a31085612391b1
amansingh-at-502661615878 <amansingh947860@gmail.com>
2025-12-06 23:54:03 +0530

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex items-center">
-                        <img src="/src/assets/logo.jpg" alt="Acadifyx Logo" className="h-10 w-auto rounded-full" />
+                        <img src="/logo.jpg" alt="Acadifyx Logo" className="h-10 w-auto rounded-full" />
                         <span className="ml-2 text-xl font-bold text-gray-900">Acadifyx</span>
                     </div>
 
